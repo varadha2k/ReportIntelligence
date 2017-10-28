9416fe991da1384c77ba2e9f32eb8eb4110d7e45
Varadharajan Srinivasan <Srinivasanva@dnb.com>
2017-10-28 21:52:50 +0530

    testing travis - 2

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ReportIntelligence
 
+[![Build Status](https://travis-ci.org/varadha2k/ReportIntellligence.svg?branch=master)](https://travis-ci.org/varadha2k/ReportIntellligence)
+
 Report Intelligence will have 3 components
 1. API to update reporting data
 2. Solution to analyze the test results
--- src/test/java/com/chennai/reportintelligence/SampleTest.java
+++ src/test/java/com/chennai/reportintelligence/SampleTest.java
@@ -1,22 +1,10 @@
 package com.chennai.reportintelligence;
 
-
 import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mapstruct.factory.Mappers;
-
 
 public class SampleTest {
 
-	@BeforeClass
-	public static void initialize() {
-	}
-
-	@Before
-	public void initializeTest() {
-	}	
 
 	@Test
 	public void test1() {
@@ -24,4 +12,9 @@ public class SampleTest {
 		
 	}
 	 
+	@Test
+	public void test2() {
+		assertEquals(new Long(10),Long(10));
+		
+	}
 }
